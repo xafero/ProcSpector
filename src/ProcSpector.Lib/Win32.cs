a867c2a988bd1166eda9efb0f45ce7c5cb434e0e
xafero <xafero@users.noreply.github.com>
2025-10-03 15:26:30 +0200

    Adding hwnd screenshot

--- src/ProcSpector.Lib/Win32.cs
+++ src/ProcSpector.Lib/Win32.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Text;
 
+#pragma warning disable CA1416
+
 namespace ProcSpector.Lib
 {
     internal static class Win32
@@ -34,17 +36,20 @@ namespace ProcSpector.Lib
         private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint dwProcessId);
 
         [StructLayout(LayoutKind.Sequential)]
-        private struct RECT
+        private struct Rect
         {
             public int Left;
             public int Top;
             public int Right;
             public int Bottom;
+
+            public int Width => Right - Left;
+            public int Height => Bottom - Top;
         }
 
         [DllImport("user32", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        private static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
 
         public static List<WinStruct> GetWindows(IntPtr? parent = null)
         {
@@ -102,6 +107,70 @@ namespace ProcSpector.Lib
                 return item;
             }
             return null;
+        }
+
+        [DllImport("user32")]
+        private static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+        [DllImport("user32")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("gdi32")]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hDc);
+
+        [DllImport("gdi32")]
+        private static extern IntPtr CreateCompatibleBitmap(IntPtr hDc, int nWidth, int nHeight);
+
+        [DllImport("gdi32")]
+        private static extern IntPtr SelectObject(IntPtr hDc, IntPtr hGdiObj);
+
+        [DllImport("gdi32")]
+        private static extern bool DeleteDC(IntPtr hDc);
+
+        [DllImport("gdi32")]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32")]
+        private static extern bool BitBlt(IntPtr hDestDc, int x, int y, int nWidth, int nHeight,
+            IntPtr hSrcDc, int xSrc, int ySrc, uint dwRop);
+
+        private const uint SrcCopy = 0x00CC0020;
+
+        public static Bitmap? CaptureWindow(IntPtr hWnd)
+        {
+            if (!GetWindowRect(hWnd, out var windowRect))
+                return null;
+
+            var width = windowRect.Width;
+            var height = windowRect.Height;
+
+            if (width <= 0 || height <= 0)
+                return null;
+
+            var windowDc = GetWindowDC(hWnd);
+            if (windowDc == IntPtr.Zero)
+                return null;
+
+            try
+            {
+                var memoryDc = CreateCompatibleDC(windowDc);
+                var bitmap = CreateCompatibleBitmap(windowDc, width, height);
+                var oldBitmap = SelectObject(memoryDc, bitmap);
+
+                BitBlt(memoryDc, 0, 0, width, height, windowDc, 0, 0, SrcCopy);
+
+                var result = Image.FromHbitmap(bitmap);
+
+                SelectObject(memoryDc, oldBitmap);
+                DeleteObject(bitmap);
+                DeleteDC(memoryDc);
+
+                return result;
+            }
+            finally
+            {
+                ReleaseDC(hWnd, windowDc);
+            }
         }
     }
 }
