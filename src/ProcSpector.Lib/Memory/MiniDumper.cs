d4d6211b6f18788318f7aa64680ad7117e0a6604
xafero <xafero@users.noreply.github.com>
2025-10-03 21:06:58 +0200

    Reformatting

--- src/ProcSpector.Lib/Memory/MiniDumper.cs
+++ src/ProcSpector.Lib/Memory/MiniDumper.cs
@@ -9,29 +9,17 @@ namespace ProcSpector.Lib.Memory
     internal static class MiniDumper
     {
         [DllImport("dbghelp", SetLastError = true)]
-        private static extern bool MiniDumpWriteDump(
-            IntPtr hProcess,
-            uint processId,
-            IntPtr hFile,
-            MiniDumpType dumpType,
-            IntPtr exceptionParam,
-            IntPtr userStreamParam,
-            IntPtr callbackParam
-        );
+        private static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, IntPtr hFile,
+            MiniDumpType dumpType, IntPtr exceptionParam, IntPtr userStreamParam, IntPtr callbackParam);
 
         public static void CreateDump(Process process, string filePath,
             MiniDumpType dumpType = MiniDumpType.MiniDumpWithFullMemory)
         {
             using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
 
-            var success = MiniDumpWriteDump(
-                process.Handle,
-                (uint)process.Id,
-                fs.SafeFileHandle.DangerousGetHandle(),
-                dumpType,
-                IntPtr.Zero,
-                IntPtr.Zero,
-                IntPtr.Zero);
+            var success = MiniDumpWriteDump(process.Handle, (uint)process.Id,
+                fs.SafeFileHandle.DangerousGetHandle(), dumpType,
+                IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
 
             if (!success)
             {
