21f5be7d8c906393781bd33a2d2fb5be70742169
xafero <xafero@users.noreply.github.com>
2025-10-04 15:55:05 +0200

    Moving this

--- src/ProcSpector.API/IProcess.cs
+++ src/ProcSpector.API/IProcess.cs
@@ -1,10 +1,19 @@
+using System;
+using ByteSizeLib;
+
 namespace ProcSpector.API
 {
     public interface IProcess
     {
         int Id { get; }
         string Name { get; }
-        
+        DateTime? StartTime { get; }
+        int Threads { get; }
+        int Handles { get; }
+        ByteSize WorkingSet { get; }
+        ByteSize PagedMem { get; }
+        string? FileName { get; }
+
         void Kill();
         void CreateMemSave();
         void CreateScreenShot();
