eff44e34cb40bd2735f5aaf0ff4880c2526584af
xafero <xafero@users.noreply.github.com>
2025-10-05 18:04:13 +0200

    Creating grpc

---
+++ src/ProcSpector.Server2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ProcSpector.Server2.Services;
+
+namespace ProcSpector.Server2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddGrpc();
+
+            var app = builder.Build();
+
+            app.MapGrpcService<GreeterService>();
+            app.MapGet("/", () => "You need a gRPC client!");
+
+            app.Run();
+        }
+    }
+}
