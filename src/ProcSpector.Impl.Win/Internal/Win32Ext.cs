71a73ffa6bb64febc36d4db3b95e3e3d7f825a0d
xafero <xafero@users.noreply.github.com>
2025-10-25 23:35:03 +0200

    Reworking this

--- src/ProcSpector.Impl.Win/Internal/Win32Ext.cs
+++ src/ProcSpector.Impl.Win/Internal/Win32Ext.cs
@@ -1,13 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
 using ProcSpector.API;
-using ProcSpector.Core;
-using ProcSpector.Impl.Net;
-using ProcSpector.Impl.Win.Memory;
 
 #pragma warning disable CA1416
 
@@ -15,108 +7,29 @@ namespace ProcSpector.Impl.Win.Internal
 {
     public static class Win32Ext
     {
-        public static WinStruct? GetMainWindow(IProcess proc)
+        public static IFile? CreateMemSave(IProcess proc, WinPlatform winPlatform)
         {
-            var all = GetBiggestWindows(proc);
-            var first = all.FirstOrDefault();
-            return first.pixel != 0 ? first.handle : null;
+            throw new NotImplementedException();
         }
 
-        private static IEnumerable<(WinStruct handle, int pixel)> GetBiggestWindows(IProcess proc)
+        public static IFile? CreateMemSave(IMemRegion mem, WinPlatform winPlatform)
         {
-            return Win32.GetWindows()
-                .Where(x => x.ProcessId == proc.Id)
-                .Select(x => (x, p: GetPixelCount(Win32.GetWindowSize(x.WindowHandle))))
-                .OrderByDescending(x => x.p);
-        }
-
-        private static int GetPixelCount(Rectangle? size)
-        {
-            int pixels;
-            if (size == null)
-                pixels = 0;
-            else
-                pixels = size.Value.Width * size.Value.Height;
-            return pixels;
-        }
-
-        public static IFile? CreateMemSave(IMemRegion region)
-        {
-            var title = $"0x{region.BaseAddress:X}";
-            var filePath = MiscExt.GetTimedFileName("Region", title, "bin");
-
-            var real = ((StdMem)region).Mem;
-            if (real.Data is not { Length: >= 1 } data)
-                return null;
-
-            var bunch = StdFile.Create(filePath, data, null);
-
-            // TODO ProcExt.OpenInShell(filePath);
-            return bunch;
+            throw new NotImplementedException();
         }
 
         public static IFile? CreateScreenShot(IProcess proc)
         {
-            var win = GetMainWindow(proc);
-            if (win == null)
-                return null;
-            return CreateScreenShot(win.WindowHandle);
+            throw new NotImplementedException();
         }
 
         public static IFile? CreateScreenShot(IHandle handle)
         {
-            var win = handle.Handle;
-            if (win == null)
-                return null;
-            return CreateScreenShot(new IntPtr(win.Value));
+            throw new NotImplementedException();
         }
 
-        private static IFile CreateScreenShot(IntPtr hWnd)
+        public static IFile? CreateMiniDump(IProcess proc, WinPlatform winPlatform)
         {
-            var title = Win32.GetWindowText(hWnd);
-            var filePath = MiscExt.GetTimedFileName("Screenshot", title, "png");
-
-            var bunch = StdFile.Create(filePath, null, stream =>
-            {
-                var format = ImageFormat.Png;
-                using var bitmap = Win32Gdi.CaptureWindow(hWnd);
-                bitmap?.Save(stream, format);
-            });
-
-            // TODO ProcExt.OpenInShell(filePath);
-            return bunch;
-        }
-
-        public static IFile CreateMiniDump(IProcess proc, ISystem sys)
-        {
-            var title = Path.GetFileNameWithoutExtension(proc.FileName);
-            var filePath = MiscExt.GetTimedFileName("MiniDump", title, "dmp");
-
-            var real = ProcExt.GetStdProc(proc, sys).Proc;
-            var bytes = MiniDumper.CreateDump(real);
-
-            var bunch = StdFile.Create(filePath, bytes, null);
-
-            // TODO ProcExt.OpenInShell(filePath);
-            return bunch;
-        }
-
-        public static IFile CreateMemSave(IProcess proc, ISystem sys)
-        {
-            var title = Path.GetFileNameWithoutExtension(proc.FileName);
-            var filePath = MiscExt.GetTimedFileName("RawMem", title, "bin");
-
-            var real = ProcExt.GetStdProc(proc, sys).Proc;
-            var regions = MemoryReader.ReadAllMemoryRegions(real);
-
-            var bunch = StdFile.Create(filePath, null, stream =>
-            {
-                foreach (var region in regions)
-                    stream.Write(region.Data);
-            });
-
-            // TODO ProcExt.OpenInShell(filePath);
-            return bunch;
+            throw new NotImplementedException();
         }
     }
 }
