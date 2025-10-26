6c9089ed95147e71aac38d90480de0dbabca5281
xafero <xafero@users.noreply.github.com>
2025-10-26 18:24:45 +0100

    Refactoring

--- src/ProcSpector.Core/AsyncTool.cs
+++ src/ProcSpector.Core/AsyncTool.cs
@@ -7,14 +7,10 @@ namespace ProcSpector.Core
         public static T? GetVal<T>(this ValueTask<T>? tsk)
         {
             if (tsk is { } task)
-                return task.IsCompleted ? task.Result : task.AsTask().GetVal();
-            return default;
-        }
-
-        public static T? GetVal<T>(this Task<T>? tsk)
-        {
-            if (tsk is { } task)
-                return task.IsCompleted ? task.Result : task.ConfigureAwait(false).GetAwaiter().GetResult();
+                if (task.IsCompleted)
+                    return task.Result;
+                else
+                    return task.ConfigureAwait(false).GetAwaiter().GetResult();
             return default;
         }
     }
