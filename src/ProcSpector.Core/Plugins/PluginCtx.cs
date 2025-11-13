a246e8098ee459fd81e9f91e913fa5c7d1b31d11
xafero <xafero@users.noreply.github.com>
2025-11-13 20:01:23 +0100

    Removing using

--- src/ProcSpector.Core/Plugins/PluginCtx.cs
+++ src/ProcSpector.Core/Plugins/PluginCtx.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using Newtonsoft.Json;
 using ProcSpector.API;
 using ProcSpector.OpenCV;
 
